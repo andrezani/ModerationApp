96c50009a6e983c0570fed3b20aa5851377e02e3
Andrea Z <andrew.zani01@gmail.com>
2024-02-21 13:21:05 +0100

    Add content moderation functionality to ModeratedModel

--- app/models/moderated_model.rb
+++ app/models/moderated_model.rb
@@ -1,2 +1,10 @@
+# app/models/moderated_model.rb
 class ModeratedModel < ApplicationRecord
+  include Moderable
+
+  def self.moderated_columns
+    # Define the columns you want to moderate for this model
+    # For example: [:content]
+    [:content]
+  end
 end
